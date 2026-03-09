9d3ab7cc68d126948e54224fdb3c40d5cda89900
jodemi41 <ivan.dicka@transfergratis.com>
2026-03-09 08:44:14 +0100

    update

--- modules/front/src/common/lib/i18n/index.ts
+++ modules/front/src/common/lib/i18n/index.ts
@@ -1,14 +1,14 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import HttpBackend from "i18next-http-backend";
-import LanguageDetector from "i18next-browser-languagedetector";
+// import LanguageDetector from "i18next-browser-languagedetector";
 import { setLocale } from "@/common/utils/i18nLocale";
 
 i18n
   // charge les fichiers de traduction depuis /locales/{{lng}}/{{ns}}.json
   .use(HttpBackend)
   // detecte la langue du navigateur / localStorage / querystring
-  .use(LanguageDetector)
+  // .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     fallbackLng: "en-CA",
